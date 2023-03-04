4d548d4a53508b64d9f85c400c9ae2a7ec900fda
Momfus <momfus@outlook.com>
2023-03-04 16:58:46 -0300

    main project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListsComponent } from './pages/post-lists/post-lists.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/post-list', pathMatch: 'full' },
+  { path: 'post-list', component: PostListsComponent },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { message: 'Page not found' },
+  },
+  { path: '**', redirectTo: '/not-found' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { PostListsComponent } from './pages/post-lists/post-lists.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PostCardComponent } from './shared/post-card/post-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListsComponent,
+    NotFoundComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

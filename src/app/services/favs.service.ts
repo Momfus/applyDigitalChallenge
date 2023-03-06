1106244a6221df9734cc90d5e8700f73a7ee27fb
Momfus <momfus@outlook.com>
2023-03-06 04:00:32 -0300

    favs servise functiosn added

--- src/app/services/favs.service.ts
+++ src/app/services/favs.service.ts
@@ -6,7 +6,47 @@ import { Post } from '../models/post.model';
 })
 export class FavsService {
 
+  postFavsList: Post[] = []
 
+  constructor() {
+    this.postFavsList = this.getFavs();
+  }
 
+  getFavs(): Post[] {
+
+    const favsJson = localStorage.getItem('favs');
+
+    if (favsJson) {
+      return JSON.parse(favsJson);
+    } else {
+      return [];
+    }
+
+  }
+
+  addPostToFavs(post: Post) {
+
+    // Just in case the post was already added
+    const isPostExists = this.postFavsList.findIndex(p => p.objectID === post.objectID);
+    if (isPostExists !== -1) {
+      return
+    }
+
+    this.postFavsList.push(post);
+    localStorage.setItem('postFavsList', JSON.stringify(this.postFavsList));
+
+  }
+
+  removePostFromFavs( objectID: string ) {
+
+    const postIndex = this.postFavsList.findIndex(p => p.objectID === objectID);
+    if( postIndex !== -1 ) {
+
+      this.postFavsList.splice(postIndex, 1);
+      localStorage.setItem('postFavsList', JSON.stringify(this.postFavsList));
+
+    }
+
+  }
 
 }
